ac6c51d5528003e974796d4e07c47e018743e936
mustafa <mstfdlk01@gmail.com>
2023-02-21 16:35:35 +0300

    End of S10

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,8 +10,8 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 export class ShoppingEditComponent {
  @ViewChild('nameInput') nameInputRef: ElementRef;
  @ViewChild('amountInput') amountInputRef: ElementRef;
-@Output()  ingredientAdded= new EventEmitter<Ingredient>();
- constructor(){}
+
+ constructor(private slService: ShoppingListService){}
 ngOnInit(){
 
 }
@@ -18,7 +19,8 @@ onAddItem(){
 const ingName=this.nameInputRef.nativeElement.value
 const ingAmount=this.amountInputRef.nativeElement.value
   const newIngredient= new Ingredient(ingName,ingAmount);
-  this.ingredientAdded.emit(newIngredient);
+  this.slService.addIngredient(newIngredient);
+
 }
 
 }
