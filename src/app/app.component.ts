7f6426424be71da27faf5c177c2f0d2df9eb7693
mustafa <mstfdlk01@gmail.com>
2023-09-13 22:24:35 +0300

    End Of S14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,6 @@ import { Component } from '@angular/core';
 
 })
 export class AppComponent {
-loadedFeature='recipe'
-
-onNavigate(feature:string){
-this.loadedFeature=feature
-}
 
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,12 +2,13 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
-  recipeSelected=new EventEmitter<Recipe>();
+ 
   private recipes:Recipe[]=[
     new Recipe('A Test Recipe','This is simply a test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
     [new Ingredient('Meat',1),new Ingredient('French Fries',20)]),
